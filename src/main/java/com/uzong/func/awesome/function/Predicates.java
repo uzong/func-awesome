6d45dafa04578e2c3b2465a874ef31973f8735a3
uzong <sky.hu@spotterio.com>
2025-01-27 15:15:01 +0800

    丰富一下 Predicates 功能

--- src/main/java/com/uzong/func/awesome/function/Predicates.java
+++ src/main/java/com/uzong/func/awesome/function/Predicates.java
@@ -23,4 +23,39 @@ public class Predicates {
     public static <T> Predicate<T> any(Predicate<T>... predicates) {
         return t -> Arrays.stream(predicates).anyMatch(p -> p.test(t));
     }
+
+    /**
+     * 通用的逻辑与操作
+     */
+    public static <T> Predicate<T> and(Predicate<T>... predicates) {
+        Predicate<T> result = t -> true;
+        for (Predicate<T> predicate : predicates) {
+            result = result.and(predicate);
+        }
+        return result;
+    }
+
+    /**
+     * 通用的逻辑或操作
+     * @param predicates
+     * @return
+     * @param <T>
+     */
+    public static <T> Predicate<T> or(Predicate<T>... predicates) {
+        Predicate<T> result = t -> false;
+        for (Predicate<T> predicate : predicates) {
+            result = result.or(predicate);
+        }
+        return result;
+    }
+
+    /**
+     * 通用的逻辑非操作
+     * @param predicate
+     * @return
+     * @param <T>
+     */
+    public static <T> Predicate<T> not(Predicate<T> predicate) {
+        return predicate.negate();
+    }
 } 
